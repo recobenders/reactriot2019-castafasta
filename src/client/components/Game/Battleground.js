b675d2de57256e42a3b61e5ea91993aaedb46970
Juraj Visnovsky <visnovsky.j@gmail.com>
2019-09-28 23:08:38 +0200

    Allow rotating animated images

--- src/client/components/Game/Battleground.js
+++ src/client/components/Game/Battleground.js
@@ -2,15 +2,38 @@ import React, { Component } from "react";
 import AnimatedWizard from "../Animations/AnimatedWizard";
 import AnimatedSpell from "../Animations/AnimatedSpell";
 import AnimatedShot from "../Animations/AnimatedShot";
+import styled from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+  margin: 15px;
+`;
 
 class Battleground extends Component {
   render() {
     return (
-      <div>
-        <AnimatedWizard wizardColor="blue" wizardAction="jump" />
-        <AnimatedSpell spellType="earth" spellPower="strong" />
-        <AnimatedShot shotType="earth" />
-      </div>
+      <Row>
+        <Column>
+          <AnimatedWizard wizardColor="blue" wizardAction="jump" direction="right" />
+          <AnimatedSpell spellType="earth" spellPower="strong" direction="right" />
+          <AnimatedShot shotType="earth" direction="right" />
+        </Column>
+        <Column>
+          <AnimatedWizard wizardColor="red" wizardAction="idle" direction="left" />
+          <AnimatedSpell spellType="ice" spellPower="medium" direction="left" />
+          <AnimatedShot shotType="ice" direction="left" />
+        </Column>
+      </Row>
     );
   }
 }
