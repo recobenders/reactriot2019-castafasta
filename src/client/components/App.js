29ae77f9696a8ff33d461abda2d39653b26d7f91
jvisnovsky <1135003+juraj-visnovsky@users.noreply.github.com>
2019-09-29 01:02:20 +0200

    Fix socket url, now for real

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -30,14 +30,14 @@ class App extends Component {
       cookies.set("user_id", userId);
     }
 
+    let socketUrl = "http://" + window.location.hostname + ":" + process.env.SOCKET_PORT
+
     if(process.env.NODE_ENV === 'production') {
-      const socketUrl = "https://" + window.location.hostname + ":" + process.env.SOCKET_PORT
-    } else {
-      const socketUrl = "http://" + window.location.hostname + ":" + process.env.SOCKET_PORT
+      socketUrl = "https://" + window.location.hostname + ":" + process.env.SOCKET_PORT
     }
 
     this.state = {
-      socket: socketIOClient("http://" + window.location.hostname + ":4001"),
+      socket: socketIOClient(socketUrl),
       userId: userId
     };
 
