aecb8bacd37078c263432b681a3b2aa4c79d66d1
Zuzana <bobotova.zuzanka@gmail.com>
2019-09-28 14:26:00 +0200

    Polish animation strusture

--- src/client/components/Pages/PC/GamePage.js
+++ src/client/components/Pages/PC/GamePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Constants from "../../../../shared/constants";
-import wizard from "./wizard.png"
+import AnimatedImage from "../../Animations/AnimatedImage";
 
 const Wrapper = styled.section`
   position: fixed;
@@ -10,44 +10,18 @@ const Wrapper = styled.section`
   transform: translate(-50%, -50%);
 `;
 
-const AnimatedImage = styled.div`
-    width: 624px;
-    height: 500px;
-    background: url('${wizard}') 0px 0px;
-`;
-
 export class GamePage extends Component {
-  imageRef;
-
-  constructor(props){
-    super(props);
-    this.imageRef = React.createRef();
-  }
-
   componentDidMount() {
     this.props.socket.on(Constants.MSG.GAME_UPDATE, () => {
       console.log('Received game update');
     });
-
-    console.log(this.imageRef.current);
-    let position = 624;
-    const interval = 150;
-    setInterval(()=> {
-      this.imageRef.current.style.backgroundPosition = `-${position}px 0px`
-
-      if(position < 4368){
-        position += 624;
-      } else {
-        position = 0;
-      }
-    }, interval);
   }
 
 
   render() {
     return (
       <Wrapper>
-        <AnimatedImage url={wizard} ref={this.imageRef}/>
+        <AnimatedImage />
       </Wrapper>
     );
   }
