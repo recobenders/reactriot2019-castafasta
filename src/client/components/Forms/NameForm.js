3552bfa5c1f150692ddc38d05bb97eb403f2ab0e
jvisnovsky <1135003+juraj-visnovsky@users.noreply.github.com>
2019-09-28 11:04:31 +0200

    Basic username form

---
+++ src/client/components/Forms/NameForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Constants from "../../../shared/constants";
+
+class NameForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.socket.emit(Constants.MSG.NEW_PLAYER, this.state.name);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default NameForm;
