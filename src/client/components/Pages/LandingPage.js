e7dd649ddfc4e5920afba31867194bf2300d97c9
jvisnovsky <1135003+juraj-visnovsky@users.noreply.github.com>
2019-09-28 08:22:46 +0200

    Dummy landing page with QR code leading nowhere

---
+++ src/client/components/Pages/LandingPage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import QRCode from 'qrcode.react';
+import { default as UUID } from 'node-uuid';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      qrCode: window.location.host + '/' + UUID.v4()
+    }
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <QRCode value={this.state.qrCode} />
+      </Wrapper>
+    );
+  }
+}
+
+export default LandingPage;
