8ee301f955356f907159085c2344feb345fd5f17
Michal Granec <michal.granec@innovatrics.com>
2019-09-29 23:19:17 +0200

    add animatino to healthbars

--- stories/Healthbar.stories.js
+++ stories/Healthbar.stories.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import HealthBar from "../src/client/components/Game/HealthBar";
+import { withKnobs, number } from "@storybook/addon-knobs";
 
 storiesOf("HealthBar", module)
+  .addDecorator(withKnobs)
   .addDecorator(story => (
     <div
       style={{
@@ -13,5 +15,5 @@ storiesOf("HealthBar", module)
     </div>
   ))
   .add("default", () => <HealthBar hp={500} tilt />)
-  .add("without tilt", () => <HealthBar hp={500} />)
+  .add("without tilt", () => <HealthBar hp={number("hp", 100)} />)
   .add("low hp", () => <HealthBar hp={40} />);
