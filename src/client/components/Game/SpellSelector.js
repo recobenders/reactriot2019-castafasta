0c862ddf73eda83c041bd2ac4966a13d66f94de4
Michal Granec <michal.granec@innovatrics.com>
2019-09-30 01:18:44 +0200

    add spell icons

--- src/client/components/Game/SpellSelector.js
+++ src/client/components/Game/SpellSelector.js
@@ -1,13 +1,74 @@
 import React, { Component } from "react";
 import { Row, Col } from "antd";
 import _ from "lodash";
-import icon from "./assets/spell_fire_fireball.jpg";
+
+import earthStrong from "./assets/earth_strong_2.png";
+import earthMedium from "./assets/earth_medium_1.png";
+import earthWeak from "./assets/earth_weak_3.png";
+
+import fireStrong from "./assets/fire_strong_2.png";
+import fireMedium from "./assets/fire_medium_1.png";
+import fireWeak from "./assets/fire_weak_3.png";
+
+import iceStrong from "./assets/ice_strong_2.png";
+import iceMedium from "./assets/ice_medium_1.png";
+import iceWeak from "./assets/ice_weak_3.png";
+
+import windStrong from "./assets/wind_strong_2.png";
+import windMedium from "./assets/wind_medium_1.png";
+import windWeak from "./assets/wind_weak_3.png";
+
 import Text from "antd/lib/typography/Text";
 
 class SpellSelector extends Component {
+  retrieveIcon(spellKey) {
+    let icon;
+    switch (spellKey) {
+      case "fireBolt":
+        icon = fireWeak;
+        break;
+      case "meteor":
+        icon = fireMedium;
+        break;
+      case "fieryImplosion":
+        icon = fireStrong;
+        break;
+      case "glacialTouch":
+        icon = iceWeak;
+        break;
+      case "iceCrash":
+        icon = iceMedium;
+        break;
+      case "frostSpike":
+        icon = iceStrong;
+        break;
+      case "whirlingBlades":
+        icon = windWeak;
+        break;
+      case "shredder":
+        icon = windMedium;
+        break;
+      case "tornado":
+        icon = windStrong;
+        break;
+      case "boulderSlam":
+        icon = earthWeak;
+        break;
+      case "talonsFromThePast":
+        icon = earthMedium;
+        break;
+      case "titanClap":
+        icon = earthStrong;
+        break;
+      default:
+        icon = fireWeak;
+    }
+    return icon;
+  }
+
   renderSpellItem(spell) {
     return (
-      <Col span={4}>
+      <Col span={6}>
         <div
           onClick={() => this.props.handleSpellSelected(spell.key)}
           key={spell.id}
@@ -17,8 +78,24 @@ class SpellSelector extends Component {
             alignItems: "center"
           }}
         >
-          <img src={icon} width="70" height="70" alt="fireball" />
-          <Text strong>{spell.name}</Text>
+          <div
+            style={{
+              background:
+                "linear-gradient(to right,  #0f0c29, #302b63, #24243e)",
+              borderRadius: "6px",
+              boxShadow: "2px 2px 3px #000000"
+            }}
+          >
+            <img
+              src={this.retrieveIcon(spell.key)}
+              width="65"
+              height="65"
+              alt={spell.key}
+            />
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <Text strong>{spell.name}</Text>
+          </div>
         </div>
       </Col>
     );
@@ -28,7 +105,7 @@ class SpellSelector extends Component {
     let preparedPairs = _.chunk(this.props.spells, 3);
     let spellRows = preparedPairs.map(triplet => {
       return (
-        <Row justify="center" type="flex" gutter={120}>
+        <Row justify="space-around" type="flex" style={{ minHeight: "110px" }}>
           {this.renderSpellItem(triplet[0])}
           {this.renderSpellItem(triplet[1])}
           {this.renderSpellItem(triplet[2])}
