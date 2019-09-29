b2b986e22c597fae82cafab3a812012071e6a4e0
Zuzana <bobotova.zuzanka@gmail.com>
2019-09-29 18:07:57 +0200

    style mobile page

--- src/client/components/Pages/Mobile/MobilePage.js
+++ src/client/components/Pages/Mobile/MobilePage.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import { withCookies } from "react-cookie";
-import NameForm from "../../Forms/NameForm";
 import styled from "styled-components";
+import NameForm from "../../Forms/NameForm";
 import Constants from "../../../../shared/constants";
 import Title from "antd/lib/typography/Title";
+import wandBlue from "../../Game/assets/wiz_wand_blue.png";
+import wandRed from "../../Game/assets/wiz_wand_red.png";
 
 const Wrapper = styled.section`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  pagination: 10px;
+`;
+
+const ImagesRow = styled.div`
+  display: flex,
+  justify-content: space-around;
+  margin-bottom: 20px;
+  align-items: flex-end
+`;
+
+const Image = styled.img`
+    width: 80px;
+    ${({left}) => `transform: rotate(${left ? -40 : 40}deg) translate(${left ? 20 : -20}px)`};
 `;
 
 class MobilePage extends Component {
@@ -40,6 +57,10 @@ class MobilePage extends Component {
   render() {
     return (
       <Wrapper>
+        <ImagesRow>
+          <Image src={wandRed} left/>
+          <Image src={wandBlue}/>
+        </ImagesRow>
         <Title level={2}>Hello, I am your wand!</Title>
         <NameForm
           handleFormSubmit={this.handleFormSubmit.bind(this)}
